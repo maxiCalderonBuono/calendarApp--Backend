b75b4ef7b9c1012d59cbfefccad96bfcc0945752
Maxi <calderonmaxi@outlook.com>
2022-08-23 11:58:26 +0200

    fix: change routes

--- index.js
+++ index.js
@@ -34,8 +34,6 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events"));
 
-app.use(express.static(path.join(__dirname, "build")));
-
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
