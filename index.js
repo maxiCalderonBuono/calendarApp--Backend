54687451e286bc4b1b4c89b24c1b6711584c30d3
Maxi <calderonmaxi@outlook.com>
2022-08-23 11:49:45 +0200

    fix: solved problem navigation with *

--- index.js
+++ index.js
@@ -33,6 +33,10 @@ app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events"));
+
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "public/index.html");
+});
 //Listening
 
 app.listen(process.env.PORT, () => {
