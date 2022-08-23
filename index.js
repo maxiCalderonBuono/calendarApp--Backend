08081bc8f6966eb2275dd48e23db308d9c83b1ce
Maxi <calderonmaxi@outlook.com>
2022-08-23 11:55:34 +0200

    fix: new change in routes

--- index.js
+++ index.js
@@ -34,9 +34,12 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events"));
 
-app.get("*", (req, res) => {
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+
 //Listening
 
 app.listen(process.env.PORT, () => {
