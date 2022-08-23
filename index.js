879a1f20f51e78124b6c006f5ebcb563f9630565
Maxi <calderonmaxi@outlook.com>
2022-08-24 01:58:46 +0200

    fix: routing

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use("/api/events", require("./routes/events"));
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/*", function (req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
