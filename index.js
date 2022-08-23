c28a7700f2608bf6780b9a943e1c9978ef39839b
Maxi <calderonmaxi@outlook.com>
2022-08-23 11:53:51 +0200

    fix: solved path join

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events"));
 
 app.get("*", (req, res) => {
-  res.sendFile(__dirname + "public/index.html");
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 //Listening
 
